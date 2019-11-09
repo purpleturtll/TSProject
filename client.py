0b30d233e2c150b3e716853c1f7f5667f6a541df
Bartosz Ciesielczyk <purpleturtll@gmail.com>
2019-11-09 17:51:21 +0100

    Sesja, historia, przesyłanie całego nagłówka

--- client.py
+++ client.py
@@ -1,9 +1,30 @@
+"""
+
+Nagłówek:
+Pole           -> |Pole Operacji|Pole Statusu|Pole Identyfikatora Sesji|  Timestamp  |Arg1 | Arg2 |
+Klucz          -> |      OP     |     ST     |           ID            |      TS     | A1  |  A2  |
+
+
+Operacje:
+potęgowanie    -> OP=poteguj$
+logarytmowanie -> OP=logarytmuj$
+dodawanie      -> OP=dodaj$
+odejmowanie    -> OP=odejmij$
+mnożenie       -> OP=mnoz$
+dzielenie      -> OP=dziel$
+historia       -> OP=historia$
+
+"""
+
+
 import socket
 import os
+import time
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect(("192.168.137.1", 8080))
+#client.connect(("192.168.137.1", 8080))
+client.connect(("127.0.0.1", 8080))
 
 client.setblocking(True)
 
@@ -16,67 +37,66 @@ def menu():
     print("5 - MNOŻENIE")
     print("6 - DZIELENIE")
     print()
-    print("WYBIERZ NUMER OPERACJI:", end = " ")
+    print("WYBIERZ NUMER OPERACJI:", end=" ")
 
 
 def pods_wykl():
-    print("PODAJ PODSTAWĘ:", end = " ")
+    print("PODAJ PODSTAWĘ:", end=" ")
     a = input()
-    print("PODAJ WYKŁADNIK:", end = " ")
+    print("PODAJ WYKŁADNIK:", end=" ")
     b = input()
     os.system('cls')
     return a, b
+
 
 def dodaw_odejm():
-    print("PODAJ PIERWSZĄ LICZBĘ:", end = " ")
+    print("PODAJ PIERWSZĄ LICZBĘ:", end=" ")
     a = input()
-    print("PODAJ DRUGĄ LICZBĘ:", end = " ")
+    print("PODAJ DRUGĄ LICZBĘ:", end=" ")
     b = input()
     os.system('cls')
     return a, b
 
+
 def mnożenie():
-    print("PODAJ PIERWSZY CZYNNIK:", end = " ")
+    print("PODAJ PIERWSZY CZYNNIK:", end=" ")
     a = input()
-    print("PODAJ DRUGI CZYNNIK:", end = " ")
+    print("PODAJ DRUGI CZYNNIK:", end=" ")
     b = input()
     os.system('cls')
-    return a,b
+    return a, b
+
 
 def dzielenie():
-    print("PODAJ DZIELNĄ:", end = " ")
+    print("PODAJ DZIELNĄ:", end=" ")
     a = input()
-    print("PODAJ DZIELNIK:", end = " ")
-    
-    while True: 
+    print("PODAJ DZIELNIK:", end=" ")
+
+    while True:
         b = input()
         if(b == '0'):
             print("DZIELNIK NIE MOŻE BYĆ ZEREM")
-            print("PODAJ POPRAWNY DZIELNIK:", end= " ")
+            print("PODAJ POPRAWNY DZIELNIK:", end=" ")
         else:
             break
-    return a,b
-
-
+    return a, b
 
 
 def param(operacja):
     os.system('cls')
 
-    if operacja =='1' or operacja == '2':
+    if operacja == '1' or operacja == '2':
         return pods_wykl()
-   
+
     if operacja == '3' or operacja == '4':
         return dodaw_odejm()
 
     if operacja == '5':
         return mnożenie()
-    
+
     if operacja == '6':
         return dzielenie()
 
-
-    
 
 def na_String(operacja):
     if operacja == '1':
@@ -94,10 +114,17 @@ def na_String(operacja):
 
     return operacja
 
-def xd(operacja, a1, a2):
-    
-    msg = "OP=" + operacja + "$A1="+a1+"$A2="+a2+"$"
+
+def dane(operacja, a1, a2):
+    msg = "OP=" + operacja + "$"
+    msg += "ST=" + str(0) + "$"
+    msg += "ID=" + SESSION_ID + "$"
+    msg += "TS=" + time.asctime(time.localtime(time.time())) + "$"
+    msg += "A1=" + a1 + "$"
+    msg += "A2=" + a2 + "$"
     return msg
+
+SESSION_ID = client.recv(1024).decode("utf-8")
 
 while True:
     os.system('cls')
@@ -105,12 +132,8 @@ while True:
     operacja = input()
     a, b = param(operacja)
     operacja = na_String(operacja)
-    msg = xd(operacja,a,b)
+    msg = dane(operacja, a, b)
     client.send(msg.encode("utf-8"))
     res = client.recv(1024)
-    OPERACJE = []
-    OPERACJE.append(res)
     print(res.decode("utf-8"))
     input()
-
- 
--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 """
 
 Nagłówek:
-Pole           -> |Pole Operacji|Pole Statusu|Pole Identyfikatora Sesji|  Timestamp  |Arg1 | Arg2 | 
-Klucz          -> |      OP     |     ST     |           ID            |      TS     | A1  |  A2  |
-Rozmiar(bajt)  -> |      14     |      4     |            4            |       8     |  4  |   4  |
+Pole           -> |Pole Operacji|Pole Statusu|Pole Identyfikatora Sesji|  Timestamp  |Arg1 | Arg2 | Wynik |
+Klucz          -> |      OP     |     ST     |           ID            |      TS     | A1  |  A2  |   WY  |
 
 
 Operacje:
@@ -13,11 +12,14 @@ dodawanie      -> OP=dodaj$
 odejmowanie    -> OP=odejmij$
 mnożenie       -> OP=mnoz$
 dzielenie      -> OP=dziel$
+historia       -> OP=historia$
 
 """
 
 import socketserver
 import math
+import time
+import threading
 
 SOCKETS = {}
 OPERATIONS = {}
@@ -29,15 +31,14 @@ class Operation:
     def __init__(self, data, result):
         global OPERATION_ID
         self.query = data
-        self.resule = result
+        self.result = result
         self.operation_id = OPERATION_ID
-        OPERATION_ID += 1
 
     def __str__(self):
-        return self.query[0][3:] + " " + self.query[1][3:] + " " + self.query[2][3:]
+        return self.query[0][3:] + " " + self.query[4][3:] + " " + self.query[5][3:] + " = " + self.result
 
 
-class Handler(socketserver.StreamRequestHandler):
+class Handler(socketserver.BaseRequestHandler):
 
     def potegowanie(self, x1, x2):
         return pow(int(x1), int(x2))
@@ -57,11 +58,34 @@ class Handler(socketserver.StreamRequestHandler):
     def dzielenie(self, x1, x2):
         return int(x1)/int(x2)
 
+    def historia(self, x1):
+        if x1 != "":
+            if int(x1) in OPERATIONS[SOCKETS[self.request]].keys():
+                return str(OPERATIONS[SOCKETS[self.request]][int(x1)])
+            else:
+                return ""
+        else:
+            o = []
+            for i in OPERATIONS[SOCKETS[self.request]]:
+                o.append(str(OPERATIONS[SOCKETS[self.request]][i]))
+            return str(";".join(o))
+
+    def dane(self, operacja, a1, a2, result, status):
+        msg = "OP=" + operacja + "$"
+        msg += "ST=" + str(status) + "$"
+        msg += "ID=" + str(SOCKETS[self.request]) + "$"
+        msg += "TS=" + time.asctime(time.localtime(time.time())) + "$"
+        msg += "A1=" + a1 + "$"
+        msg += "A2=" + a2 + "$"
+        msg += "WY=" + result + "$"
+        return msg
+
     def handle(self):
-        global SOCKETS, SESSION_ID, OPERATIONS
+        global SOCKETS, SESSION_ID, OPERATIONS, OPERATION_ID
         SOCKETS[self.request] = SESSION_ID
-        OPERATIONS[SOCKETS[self.request]] = []
+        OPERATIONS[SOCKETS[self.request]] = {}
         SESSION_ID += 1
+        self.request.send(bytes(str(SOCKETS[self.request]), "utf-8"))
         while(True):
             data = self.request.recv(1024)
             if not data:
@@ -69,34 +93,50 @@ class Handler(socketserver.StreamRequestHandler):
             data = data.decode("utf-8")
             data = data.split("$")
             print(data)
-            if data[0][3:] == "poteguj":
-                result = str(self.potegowanie(data[1][3:], data[2][3:]))
-                self.request.send(bytes(result, "utf-8"))
-            if data[0][3:] == "logarytmuj":
-                result = str(self.logarytmowanie(data[1][3:], data[2][3:]))
-                self.request.send(bytes(result, "utf-8"))
-            if data[0][3:] == "dodaj":
-                result = str(self.dodawanie(data[1][3:], data[2][3:]))
-                self.request.send(bytes(result, "utf-8"))
-            if data[0][3:] == "odejmij":
-                result = str(self.odejmowanie(data[1][3:], data[2][3:]))
-                self.request.send(bytes(result, "utf-8"))
-            if data[0][3:] == "mnoz":
-                result = str(self.mnozenie(data[1][3:], data[2][3:]))
-                self.request.send(bytes(result, "utf-8"))
-            if data[0][3:] == "dziel":
-                result = str(self.dzielenie(data[1][3:], data[2][3:]))
-                self.request.send(bytes(result, "utf-8"))
-            OPERATIONS[SOCKETS[self.request]] = Operation(data, result)
-            print(OPERATIONS[SOCKETS[self.request]])
+
+            if data[2][3:] != str(SOCKETS[self.request]):
+                status = 1
+            else:
+                status = 0
+
+            if status == 0:
+                if data[0][3:] == "poteguj":
+                    result = str(self.potegowanie(data[4][3:], data[5][3:]))
+                if data[0][3:] == "logarytmuj":
+                    result = str(self.logarytmowanie(data[4][3:], data[5][3:]))
+                if data[0][3:] == "dodaj":
+                    result = str(self.dodawanie(data[4][3:], data[5][3:]))
+                if data[0][3:] == "odejmij":
+                    result = str(self.odejmowanie(data[4][3:], data[5][3:]))
+                if data[0][3:] == "mnoz":
+                    result = str(self.mnozenie(data[4][3:], data[5][3:]))
+                if data[0][3:] == "dziel":
+                    result = str(self.dzielenie(data[4][3:], data[5][3:]))
+                if data[0][3:] == "historia":
+                    result = str(self.historia(data[4][3:]))
+                else:
+                    OPERATIONS[SOCKETS[self.request]
+                               ][OPERATION_ID] = Operation(data, result)
+                    OPERATION_ID += 1
+            else:
+                result = ""
+            self.request.send(
+                bytes(self.dane(data[0][3:], data[4][3:], data[5][3:], result, status), "utf-8"))
 
 
-class MyTCPServer(socketserver.TCPServer):
-    allow_reuse_address = True
+class MyTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
 
 
-HOST, PORT = "192.168.137.1", 8080
+#HOST, PORT = "192.168.137.1", 8080
+HOST, PORT = "127.0.0.1", 8080
 
 server = MyTCPServer((HOST, PORT), Handler)
 
-server.serve_forever()
+# server.serve_forever()
+
+with server:
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.daemon = True
+    server_thread.start()
+    time.sleep(math.pow(10, 6))
