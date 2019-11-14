d24ad0197ebe162c708fa30cd5c1d88f3ffad35a
annaciszak <aniac600@gmail.com>
2019-11-14 17:41:56 +0100

    HIstoria po stronie klienta

--- client.py
+++ client.py
@@ -23,8 +23,8 @@ import time
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#client.connect(("192.168.137.1", 8080))
-client.connect(("127.0.0.1", 8080))
+client.connect(("192.168.137.1", 8080))
+# client.connect(("127.0.0.1", 8080))
 
 client.setblocking(True)
 
@@ -36,6 +36,9 @@ def menu():
     print("4 - ODEJMOWANIE")
     print("5 - MNOŻENIE")
     print("6 - DZIELENIE")
+    print("7 - PEŁNA HISTORIA")
+    print("8 - HISTORIA PO ID")
+    print("9 - WYJDŹ")
     print()
     print("WYBIERZ NUMER OPERACJI:", end=" ")
 
@@ -45,7 +48,6 @@ def pods_wykl():
     a = input()
     print("PODAJ WYKŁADNIK:", end=" ")
     b = input()
-    os.system('cls')
     return a, b
 
 
@@ -54,7 +56,6 @@ def dodaw_odejm():
     a = input()
     print("PODAJ DRUGĄ LICZBĘ:", end=" ")
     b = input()
-    os.system('cls')
     return a, b
 
 
@@ -63,7 +64,6 @@ def mnożenie():
     a = input()
     print("PODAJ DRUGI CZYNNIK:", end=" ")
     b = input()
-    os.system('cls')
     return a, b
 
 
@@ -82,8 +82,21 @@ def dzielenie():
     return a, b
 
 
+def całaHis():
+    a, b ="", ""
+    return a, b
+
+
+def HistID():
+    print("PODAJ ID: ", end=" ")
+    a = input()
+    b = ""
+    return a, b    
+
+
+
 def param(operacja):
-    os.system('cls')
+  #  os.system('cls')
 
     if operacja == '1' or operacja == '2':
         return pods_wykl()
@@ -97,9 +110,17 @@ def param(operacja):
     if operacja == '6':
         return dzielenie()
 
+    if operacja == '7':
+        return całaHis()
+
+    if operacja == '8':
+        return HistID()
+
+
 
 def na_String(operacja):
     if operacja == '1':
+
         operacja = "poteguj"
     if operacja == '2':
         operacja = "logarytmuj"
@@ -111,6 +132,8 @@ def na_String(operacja):
         operacja = "mnoz"
     if operacja == '6':
         operacja = "dziel"
+    if operacja == '7' or operacja == '8':
+        operacja = "historia"
 
     return operacja
 
@@ -124,16 +147,62 @@ def dane(operacja, a1, a2):
     msg += "A2=" + a2 + "$"
     return msg
 
+def dzielOdp(res):
+    wynik = res[-2][3:]
+    return wynik
+
+def historiaOgolna(res):
+    data = res[6][3:].split(";")
+    for i in range(len(data)):
+        data[i] = data[i].split(" ")
+    for i in range(len(data)):
+        print(str(i+1) + "." + data[i][0])
+        print("Argument nr 1: " + data[i][1])
+        print("Argument nr 2: " + data[i][2])
+        print("Wynik: " + data[i][3])
+
+
+def historiaID(res):
+    res = res.split(" ")
+    print("OPERACJA: "+ res[0][3:])
+    print("ARGUMENT NR 1: "+ res[1])
+    print("ARGUMENT NR 2: "+ res[2])
+    print("WYNIK: "+ res[3])
+
+
 SESSION_ID = client.recv(1024).decode("utf-8")
 
 while True:
-    os.system('cls')
     menu()
-    operacja = input()
+    operacja = '0'
+    while int(operacja) > 9 or int(operacja) < 1:
+        operacja = input()
+        if int(operacja) > 9 or int(operacja) < 1: 
+            print("NIEPOPRAWNY NUMER OPERACJI")
+            input()
+            os.system('cls')
+            menu()
+        else:
+            break
+    if operacja == '9':
+        client.close()
+        print()
+        break
+    os.system('cls')
     a, b = param(operacja)
     operacja = na_String(operacja)
     msg = dane(operacja, a, b)
     client.send(msg.encode("utf-8"))
     res = client.recv(1024)
-    print(res.decode("utf-8"))
+    res = res.decode("utf-8")
+    res = res.split("$")
+    if res[0][3:] != "historia":
+        wynik = dzielOdp(res)
+        print("WYNIK = "+ wynik)
+    else:
+        if(res[4][3:] ==""):
+            historiaOgolna(res)
+        else:
+            historiaID(res[6])
     input()
+    os.system('cls')
